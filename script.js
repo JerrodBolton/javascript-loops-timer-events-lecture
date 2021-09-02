4a8ed99394d3861701b07d5066c9109d561307a2
JerrodBolton <rodrod1@JerrodBltonsMBP.hsd1.tn.comcast.net>
2021-09-02 14:21:22 -0500

    added interval

--- script.js
+++ script.js
@@ -19,7 +19,45 @@ for (let idx = 1; idx <= 10; idx++){
 alert("This is done");
 
 
-while(){
+// let x = 10;
+// while( x <= 10){
+
+//     console.log(`Make is count ${idx}`);
+//     x--;
+// };
+// I want to keep asking a person a question over and over again.
+// let whileNotEddie = true; //flag
+// while(whileNotEddie){
+
+//     //  
+//     let coolName = prompt("Enter a cool a name (i recommend u ues Eddie)").toUpperCase();
+
+//     // Did they enter the name we wanted?
+//     if (coolName === "EDDIE"){
+//         whileNotEddie = true;
+//     }
+// };
+
+
+// console.log(`BREAK OFF THE ATTACK! Their shield are still up!`);
+
+
+// setTimeout(itsATrap,3000); // A 1000 milliseconds is one sec 
+// // this is our callback function
+// function itsATrap(evt){ 
+//     // just want to add a 
+//     alert(" It's a Trap");
+
+// }
+
+// Repeating timing 
+let mytimer = setInterval(myTimer, 1000);
+
+let kounter = 0; 
+
+function myTimer(){
+    const d = new Date();
     
-    console.log(`Make is count ${idx}`);
-};
+
+
+}
